acf28e5090b3e0c45238d935fcb3d0db8bfeb8f3
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2019-03-14 16:46:07 +0000

    add a spec to check for records that aren't events or aggregates

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   # See spec/models/shared_examples/event.rb for tests of implementing classes
+
+  it 'is the default superclass for all actions and Aggregate is the same for aggregates' do
+    records_that_wont_be_event_sourced = []
+    (ApplicationRecord.descendants - records_that_wont_be_event_sourced).each do |klass|
+      expect(klass.ancestors).to include(Event).or include(Aggregate) 
+    end
+  end
 end
