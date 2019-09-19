dec624c4311b5e7402b739218b23c8b63f484500
brenetic <b@brenetic.com>
2019-09-19 16:36:47 +0100

    Correctly cater for multiple user inputs in certificate upload
    We only want to choose the option that the user has defined when they
    click on the forms radio button.
    
    Also we now correctly strip out the actual certificate from a fully
    generated .crt file.

--- app/models/concerns/certificate_concern.rb
+++ app/models/concerns/certificate_concern.rb
@@ -1,9 +1,18 @@
 module CertificateConcern
   extend ActiveSupport::Concern
 
+  BEGIN_CERT = '-----BEGIN CERTIFICATE-----'.freeze
+  END_CERT = '-----END CERTIFICATE-----'.freeze
+
   def to_x509(value)
     OpenSSL::X509::Certificate.new(value)
   rescue # rubocop:disable Style/RescueStandardError
     OpenSSL::X509::Certificate.new(Base64.decode64(value))
+  rescue # rubocop:disable Style/RescueStandardError
+    OpenSSL::X509::Certificate.new(strip_cert(value))
+  end
+
+  def strip_cert(value)
+    value.split(BEGIN_CERT).last.split(END_CERT).first
   end
 end
