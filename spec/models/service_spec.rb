1644f7c464478ba9c7997f3125cdd612f6645de7
Alan Carter <alan.carter@digital.cabinet-office.gov.uk>
2019-10-11 17:06:47 +0100

    WIP: Fix service tests

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Service, type: :model do
   context 'adding components to a service' do
-    let(:sp_component) { create(:sp_component) }
-    let(:msa_component) { create(:msa_component) }
-    let(:service) { create(:service, sp_component: sp_component) }
+    let(:sp_component1) { create(:sp_component) }
+    let(:sp_component2) { create(:sp_component) }
+    let(:msa_component1) { create(:msa_component) }
+    let(:msa_component2) { create(:msa_component) }
+    let(:service) { create(:service, sp_components: [sp_component1]) }
 
     it 'should create a new service correctly' do
       expect(service).to be_valid
       expect(service).to be_persisted
     end
 
-    it 'should be able to have an sp and msa component' do
-      service.msa_component = msa_component
+    it 'should be able to have mutiple sp and msa components' do
+      service.sp_components << sp_component2
+      service.msa_components << msa_component1
+      service.msa_components << msa_component2
       expect(service).to be_valid
       expect(service).to be_persisted
     end
