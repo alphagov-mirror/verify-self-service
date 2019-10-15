b569e578503b4b1afc0997da653b616aa20f8701
Alan Carter <alan.carter@digital.cabinet-office.gov.uk>
2019-10-15 10:38:47 +0100

    WIP - Get rid of obsolete (failing!) tests

--- app/models/new_service_event.rb
+++ app/models/new_service_event.rb
@@ -1,6 +1,6 @@
 class NewServiceEvent < AggregatedEvent
   belongs_to_aggregate :service
-  data_attributes :entity_id, :sp_component_id, :msa_component_id, :name
+  data_attributes :entity_id, :name
   validates_presence_of :entity_id, message: 'ID is required'
   validate :name_is_present
 
@@ -11,8 +11,6 @@ class NewServiceEvent < AggregatedEvent
   def attributes_to_apply
     {
       entity_id: entity_id,
-      sp_component_id: sp_component_id,
-      msa_component_id: msa_component_id,
       name: name,
       created_at: created_at,
     }
